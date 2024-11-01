d4e4e33247b6635d333d015d3cb34e12b399a4f3
akshat4675 <akshat4675@gmail.com>
2024-11-01 15:43:49 +0530

    Upadated

--- src/components/auth/confirmPage.tsx
+++ src/components/auth/confirmPage.tsx
@@ -39,7 +39,7 @@ const confirmPage = () => {
 
 return (
 
-  <div className="flex items-center justify-center min-h-screen bg-gray-100">
+  <div className="flex items-center justify-center min-h-screen bg-slate-800">
     <Card className="w-full max-w-md">
         <CardHeader className="space-y-1">
           <CardTitle className="text-2xl font-bold text-center">Confirm Account</CardTitle>
